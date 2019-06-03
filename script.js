52522e671258c2ad2a20b6c422c3c2dc28b17edf
Martinius B. Tomter <martinius.tomter@me.com>
2019-06-03 19:37:17 +0200

    Fix bug from hell and add comb generator with child elements

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
+/*document.getElementById("newCombBtn").addEventListener("click", function() {
+	var hex = document.createElement("div");
+	hex.classList.add("hexagon");
+	document.getElementById("generatorField").appendChild(hex);
+});*/
+
 document.getElementById("newCombBtn").addEventListener("click", function() {
-	var btn = document.createElement("div");
-	btn.classList.add("hexagon");
-	document.getElementById("generatorField").appendChild(btn);
+
+	var hex = document.createElement("div");
+	hex.className = "hexagon";
+
+	var like = document.createElement("button");
+	like.innerHTML = "+";
+	like.id = "likebtn";
+
+	var del = document.createElement("button");
+	del.innerHTML = "-";
+	del.id = "deletebtn";
+
+	var inp = document.createElement("input");
+	inp.className = "input";
+	inp.placeholder = "Title";
+
+	hex.appendChild(like);
+	hex.appendChild(del);
+	hex.appendChild(inp);
+	document.getElementById("generatorField").appendChild(hex);
 });
